1221b8b275169a40c46d03550fd543c62107d021
Franklin <villalta.icc@gmail.com>
2017-06-06 19:04:29 -0600

    creacion del archivo json que contiene nombre, cuenta y correo

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Examen' });
+});
+
+router.get('/0816199400127', function(req,res,next){
+  var usuario = [
+                {"nombre":"Franklin Noel Villalta Lopez"},
+                {"cuenta":"0816199400127"},
+                {"correo":"villalta.icc@gmail.com"}
+              ];
+  res.json(usuario);
 });
 
 module.exports = router;
