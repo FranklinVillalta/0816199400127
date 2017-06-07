0dceba2690e78b556a1a8ff76aa0c31505b656d8
Franklin <villalta.icc@gmail.com>
2017-06-06 19:45:00 -0600

    suma

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,28 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Examen' });
 });
 
+router.get('/Suma', function(req,res,next){
+  res.render('Suma',{"txtNum1":"","msg":""});
+
+});
+
+var usersRegistered = [];
+
+router.post('/Suma',function(req,res,next){
+  console.log(req.body);
+    usersRegistered.push(req.body.txtNum1);
+    usersRegistered.push(req.body.txtNum2);
+    // Para que conozcan map funcion de un arreglo
+    //var msgs = usersRegistered.map(function(item,i){return item}).join("|");
+    //var msgs = usersRegistered.join("|");
+    var msgs = usersRegistered;
+    var rtObject = {}; // {"txtEmail": req.body.txtEmail, "msg":msgs};
+    rtObject.txtNum1 = req.body.txtNum1;
+    rtObject.msg= msgs;
+  res.render('Suma', rtObject);
+});
+
+
 router.get('/0816199400127', function(req,res,next){
   var usuario = [
                 {"nombre":"Franklin Noel Villalta Lopez"},
